225667701ccdeeaa4e09b6033914a3766197b27d
lemachegabriel <lemache35@gmail.com>
2022-06-27 11:45:32 -0300

    finish

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Form, Row } from "react-bootstrap";
+import { Form, Row, Button } from "react-bootstrap";
 
 function SendMessage(){
     const [name, setName] = useState('')
@@ -8,18 +8,13 @@ function SendMessage(){
     const [message, setMessage] = useState('')
     const [error, setError] = useState({error:'', field:''})
 
-    const send = () => {
-        console.log('Name: %d', name)
-        console.log('Email: %d', email)
-        console.log('Tel: %d', tel)
-        console.log('Message: %d', message)
-    }
     const validate = () => {
         const emailRegex =  /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
         const emailConfirm = emailRegex.test(email)
-        if(!emailConfirm){ setError({error: 'Invalid email', field: 'email'}); return false}
 
         if(name == ''){ setError({error: 'Invalid name', field: 'name'}); return false}
+
+        if(!emailConfirm){ setError({error: 'Invalid email', field: 'email'}); return false}
 
         if(tel == ''){ setError({error: 'Invalid tel', field: 'tel'}); return false}
 
@@ -27,26 +22,44 @@ function SendMessage(){
 
         return true
     }
+    const send = () => {
+        if(validate()){
+            console.log('Name: %s \nEmail: %s\nTel: %s\nMessage: %s', name, email, tel, message)    
+        }
+    }
 
     return(
-        <Row style={{width: '50%', margin: 'auto'}}>
+        <Row style={{width: '50%', margin: 'auto', marginBottom: '50px'}}>
             <Form>
+                {error.field == 'name' && (
+                    <div style={{ fontSize: 15, color: "red", fontFamily: 'Poppins, sans-serif' }}>{error.error}</div>
+                )}
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>*Name / Organization</Form.Label>
-                    <Form.Control type="text"  />
+                    <Form.Control type="text" value={name} onChange={(e)=>setName(e.target.value)} />
                 </Form.Group>
+                {error.field == 'tel' && (
+                    <div style={{ fontSize: 15, color: "red", fontFamily: 'Poppins, sans-serif' }}>{error.error}</div>
+                )}
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>*Telephone</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" value={tel} onChange={(e)=>setTel(e.target.value)}/>
                 </Form.Group>
+                {error.field == 'email' && (
+                    <div style={{ fontSize: 15, color: "red", fontFamily: 'Poppins, sans-serif' }}>{error.error}</div>
+                )}
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>*Email Address</Form.Label>
-                    <Form.Control type="email"  />
+                    <Form.Control type="email"  value={email} onChange={(e)=>setEmail(e.target.value)}/>
                 </Form.Group>
+                {error.field == 'message' && (
+                    <div style={{ fontSize: 15, color: "red", fontFamily: 'Poppins, sans-serif' }}>{error.error}</div>
+                )}
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>*Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} value={message} onChange={(e)=>setMessage(e.target.value)}/>
                 </Form.Group>
+                <Button variant="primary" style={{width: '80px', float: 'right'}} onClick={()=>send()}>Send</Button>{' '}
             </Form>
         </Row>
     )
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/navBar"
 import { Container, Row, Col, Card } from "react-bootstrap";
+import office from '../images/about/office.jpg'
+import {FaUserCircle} from 'react-icons/fa'
+import './styles/about.css'
+import Footer from "../components/footer";
 
 function About(){
+    const lines = [1, 2, 3]
     return(
         <>
             <NavBar/>
-          
+            <Container>
+                <Row style={{marginTop: "45px", marginBottom: "35px"}}>
+                    <Col style={{textAlign: 'center'}}>
+                        <span className="titleWork">About Fetchly</span>
+                    </Col>
+                </Row>
+                <Row style={{width: "80%", margin: "auto", marginBottom: "60px"}}>
+                    <img src={office}/>
+                </Row>
+                <Row className="dataBox">
+                    {lines.map((val, ind) => {
+                        return(
+                            <Row style={{width: "85%", margin: "auto"}}>
+                        <Col className="peoples">
+                            <FaUserCircle />
+                            <p><strong>Gabriel</strong></p>
+                            <p>Software Developer</p>
+                        </Col>
+                        <Col className="peoples">
+                            <FaUserCircle />
+                            <p><strong>Gabriel</strong></p>
+                            <p>Software Developer</p>
+                        </Col>
+                        <Col className="peoples">
+                        <FaUserCircle />
+                        <p><strong>Gabriel</strong></p>
+                            <p>Software Developer</p>
+                        </Col>
+                    </Row>
+                        )
+                    })}  
+                </Row>
+            </Container>
+            <Footer/>
         </>
     )
 }
